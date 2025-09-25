65b33f8cf170d3bcab02642e05641301e0318abe
jithendranadh <jithendranadhmadaparthi@gmail.com>
2025-09-25 21:18:04 +0530

    Create Ch1_Excercises.java

---
+++ Ch1_Excercises.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        String name="Jithendranadh";
+        String position="Park Manager";
+        System.out.println("My name is "+name+" and I want to be a "+position+" in Mesozoic Eden");
+        System.out.println("Welcome Mesozoic Eden");
+
+        int number=5;
+        int doubled=doubleNumber(number);
+        System.out.println("The doubled number is : "+doubled);
+        String openingHours="08:00";
+        String closingHours="20:00";
+        System.out.println("Park's opening and closing hours are "+openingHours+" and "+closingHours);
+        System.out.println("Welcome "+name+" to Mesozoic Eden");
+        System.out.println("Mesozoic Eden is safe and secure");
+        System.out.println("Surprise Surprise,"+name);
+    }
+    public  static  int doubleNumber(int number){
+        return  number*2;
+    }
+}
