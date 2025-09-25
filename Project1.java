372054dfbf7f6d5e7cc33015037072533ab2c218
jithendranadh <jithendranadhmadaparthi@gmail.com>
2025-09-25 21:20:26 +0530

    Create Project1.java

---
+++ Project1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("Welcome to Mesozoic Eden");
+    String openingHours = "08:00";
+    String closingHours = "20:00";
+    System.out.println("Opens at "+openingHours+" and closes at "+closingHours);
+    System.out.println("Mesozoic Eden is safe and secure");
+    }
+}
