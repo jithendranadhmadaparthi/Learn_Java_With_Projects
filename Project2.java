d9f46c61d6632cb22c94935792ae4c491aba9c1c
jithendranadh <jithendranadhmadaparthi@gmail.com>
2025-09-26 23:00:37 +0530

    Create Project2.java

---
+++ Project2.java
@@ -0,0 +1,11 @@
+public class ProfileGenerator {
+    public static void main(String[] args) {
+        String dinoName = "Rexy";
+        int dinoAge = 7; //in years
+        String species = "T-Rex";
+        String diet = "Carnivore";
+        double weight = 8000; //in kilograms
+        System.out.println("Meet " + dinoName + ", a " + dinoAge + "-year-old " + species + ". As a " + diet + ", it has a robust weight of " + weight + " kilograms.");
+
+    }
+}
